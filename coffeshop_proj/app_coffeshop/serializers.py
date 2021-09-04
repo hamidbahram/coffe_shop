9ebcdab3eec5815edfb1b114d4967a8097ee2b6b
mrz <1tapciran@gmail.com>
2021-09-04 09:01:05 +0430

    improve apis

--- coffeshop_proj/app_coffeshop/serializers.py
+++ coffeshop_proj/app_coffeshop/serializers.py
@@ -3,16 +3,20 @@ from .models import Product, Option, Order
 
 
 class ProductListSerializer(serializers.ModelSerializer):
+    options = serializers.SerializerMethodField()
+
+    def get_options(self, obj):
+        return OptionListSerializer(obj.option_set.all(), many=True).data
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ['id', 'title', 'options']
 
 
 class OptionListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
-        fields = "__all__"
-        # fields = ("product","ingredients")
+        exclude = ('product', )
 
 
 class OrderListSerializer(serializers.ModelSerializer):

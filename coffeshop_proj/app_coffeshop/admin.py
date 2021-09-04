41674245453977a3996ee23b016165a18b2f85e5
bahrampour <bahrampour@hpds.ir>
2021-09-04 16:27:15 +0430

    admin

--- coffeshop_proj/app_coffeshop/admin.py
+++ coffeshop_proj/app_coffeshop/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 from .models import Product, Option, Order, OptionValue
 
 
+
+class AdminProject(admin.ModelAdmin):
+    readonly_fields = []
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            if request.user.is_superuser:
+                return self.readonly_fields 
+            else:
+                return self.readonly_fields + ['user', 'product', '_status']
+        else:
+            return self.readonly_fields + ['user', 'product', '_status']
+
+
 admin.site.register(Product)
 admin.site.register(Option)
 admin.site.register(OptionValue)
-admin.site.register(Order)
+admin.site.register(Order, AdminProject)

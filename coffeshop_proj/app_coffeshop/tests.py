8a015e721b2276f34833f68c7f1f18837f4d4fd2
bahrampour <bahrampour@hpds.ir>
2021-09-13 16:57:50 +0430

    test

--- coffeshop_proj/app_coffeshop/tests.py
+++ coffeshop_proj/app_coffeshop/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from app_coffeshop.models import Product
 
-# Create your tests here.
+
+class ModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(title='sperso')
+
+    def test_title_label(self):
+        product = Product.objects.get(title='sperso')
+        field_label = product._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
+
+
+# class YourTestClass(TestCase):
+#     def setUp(self):
+#         # Setup run before every test method.
+#         pass
+
+#     def tearDown(self):
+#         # Clean up run after every test method.
+#         pass
+
+#     def test_something_that_will_pass(self):
+#         self.assertFalse(False)
+
+#     def test_something_that_will_fail(self):
+#         self.assertTrue(False)
